a56a17a1acb00b34e7664f7a3ce3ee45d33deb45
Leon Martin <Leon@Home>
2022-12-15 22:55:52 +0100

    quicksave

--- aoc2022/day_06/script.py
+++ aoc2022/day_06/script.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python3
 
 with open("input", "r") as eingabedatei:
-    zeilen = [zeile.replace("\n", "") for zeile in eingabedatei.readlines()]
-    anzahl_spalten = int((len(zeilen[0]) + 1) / 4)
+    for zeile in eingabedatei:
+        unten, oben = 0, 4
+        
+        while oben < len(zeile):
+            if len(zeile[unten:oben]) == len(set(zeile[unten:oben])):
+                print(oben)
+                break
+            unten += 1
+            oben += 1
 
-    spalten = [[] for _ in range(anzahl_spalten)]
-    spalten_zwei = [[] for _ in range(anzahl_spalten)]
+        unten_zwei, oben_zwei = 0, 14
 
-    for zeile in zeilen:
-        if "[" in zeile:
-            for spalten_index, index in enumerate(range(1, len(zeile), 4)):
-                if zeile[index] != " ":
-                    spalten[spalten_index] = [zeile[index]] + spalten[spalten_index]
-                    spalten_zwei[spalten_index] = [zeile[index]] + spalten_zwei[spalten_index]
+        while oben_zwei < len(zeile):
+            if len(zeile[unten_zwei:oben_zwei]) == len(set(zeile[unten_zwei:oben_zwei])):
+                print(oben_zwei)
+                break
+            unten_zwei += 1
+            oben_zwei += 1
 
-        if zeile.startswith("move"):
-            _, bewege, _, von, _, nach = zeile.split(" ")
-            bewege, von, nach = int(bewege), int(von), int(nach)
-
-            for _ in range (bewege):
-                element = spalten[von-1].pop()
-                spalten[nach-1] += [element]
-
-            kisten = spalten_zwei[von-1][-bewege:]
-            spalten_zwei[von-1] = spalten_zwei[von-1][:len(spalten_zwei[von-1])-bewege]
-            spalten_zwei[nach-1] += kisten
     
-    print("".join([spalte.pop() for spalte in spalten if spalte]))
-    print("".join([spalte.pop() for spalte in spalten_zwei if spalte]))
+
+        
